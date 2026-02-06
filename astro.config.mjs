5d27224af6cc0cd6ace63ffdc8b068a474484306
shwangcmt <shwangcmt@gmail.com>
2026-02-05 23:50:04 -0800

    update theme and settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightThemeFlexoki from 'starlight-theme-flexoki';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
@@ -21,6 +24,34 @@ export default defineConfig({
 					autogenerate: { directory: 'reference' },
 				},
 			],
+			plugins: [
+				starlightThemeFlexoki({
+					accentColor: 'cyan',
+				}),
+			],
+			customCss: [
+				// Fonts
+				'@fontsource/source-serif-4/400.css',
+				'@fontsource/source-serif-4/400-italic.css',
+				'@fontsource/source-serif-4/600.css',
+				'@fontsource/source-serif-4/600-italic.css',
+				'@fontsource/source-serif-4/700.css',
+				'@fontsource/source-serif-4/700-italic.css', /* Optional but good for consistency */
+				'@fontsource/source-serif-4/900.css',
+				'@fontsource/source-sans-3/400.css',
+				'@fontsource/source-sans-3/400-italic.css',
+				'@fontsource/source-sans-3/600.css',
+				'@fontsource/source-sans-3/600-italic.css',
+				'@fontsource/source-sans-3/900.css',
+				// Custom Styles
+				'./src/styles/custom.css',
+				// KaTeX CSS
+				'katex/dist/katex.min.css',
+			],
 		}),
 	],
+	markdown: {
+		remarkPlugins: [remarkMath],
+		rehypePlugins: [rehypeKatex],
+	},
 });
