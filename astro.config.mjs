486eed554f2d8878e2e5f45c9b3036a5ec6f1ced
shwangcmt <shwangcmt@gmail.com>
2026-02-15 15:13:17 -0800

    feat: add 64x64 favicon for high-res displays

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,6 +17,7 @@ export default defineConfig({
 				// Add to home screen icons
 				{ tag: 'link', attrs: { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' } },
 				{ tag: 'link', attrs: { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' } },
+				{ tag: 'link', attrs: { rel: 'icon', type: 'image/png', sizes: '64x64', href: '/favicon-64x64.png' } },
 				{ tag: 'link', attrs: { rel: 'manifest', href: '/site.webmanifest' } },
 				// theme-color meta tags are managed dynamically by ThemeSelect.astro
 			],
