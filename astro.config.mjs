8949ed60b6a84af4e65faee09aa7d5d44a373597
shwangcmt <shwangcmt@gmail.com>
2026-02-14 22:13:46 -0800

    refactor: remove 16x16 favicon to prefer 32x32

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,7 +17,6 @@ export default defineConfig({
 				// Add to home screen icons
 				{ tag: 'link', attrs: { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' } },
 				{ tag: 'link', attrs: { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' } },
-				{ tag: 'link', attrs: { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' } },
 				{ tag: 'link', attrs: { rel: 'manifest', href: '/site.webmanifest' } },
 				// theme-color meta tags are managed dynamically by ThemeSelect.astro
 			],
