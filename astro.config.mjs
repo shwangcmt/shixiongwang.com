6a181cb6f911a81d005acb108c29afbe56685489
shwangcmt <shwangcmt@gmail.com>
2026-03-14 13:06:18 -0700

    fix course title

--- astro.config.mjs
+++ astro.config.mjs
@@ -57,6 +57,7 @@ export default defineConfig({
 			],
 			components: {
 				Head: './src/components/overrides/Head.astro',
+				SiteTitle: './src/components/overrides/SiteTitle.astro',
 				ThemeSelect: './src/components/overrides/ThemeSelect.astro',
 				Sidebar: './src/components/overrides/Sidebar.astro',
 				Pagination: './src/components/overrides/Pagination.astro',
